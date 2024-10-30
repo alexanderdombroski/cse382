a3e6827452a027c89f26d03a68cb9dd2f3ed1b3c
alexanderdombroski <dom21013@byui.edu>
2024-10-30 14:48:48 -0600

    added some changes 😅

--- ral/src/ral.erl
+++ ral/src/ral.erl
@@ -1,29 +1,45 @@
 -module(ral).
 
--export([get/1, find_tree/1, build_traversal_list/1, find_using_traversal_list/1, update/1, replace_using_traversal_list/1, cons/2, merge/2]).
-
--type random_access_list() :: list(parent_node() | leaf_node()).
-
--type leaf_node() :: {1, integer(), nil, nil}.
-
-% { number of leaf nodes, node, node } 
--type parent_node() :: {integer(), parent_node() | leaf_node(), parent_node() | leaf_node()}.
-
--spec get(integer()) -> integer().
-get([]) -> nil;
-get(_) -> ok.
-
--spec find_tree(integer()) -> tuple().
-find_tree(_) -> ok.
+-export([get/2, find_tree/2, build_traversal_list/1, find_using_traversal_list/2, update/1, replace_using_traversal_list/1, cons/2]).
 
 
--spec build_traversal_list(integer()) -> tuple().
+
+
+-type random_access_list() :: list(node()).
+
+-type ral_node() :: {integer(), integer() | nil, ral_node() | nil, ral_node() | nil}.
+
+
+% Takes an index and a random access list and returns the value at that index
+-spec get(integer(), random_access_list()) -> integer().
+get(_, []) -> nil;
+get(Index, [{Count, _, _, _} | T]) when Index > Count -> 
+    get(Index, T);
+get(Index, [{Count, V, L, R} | _]) ->
+    IntPath = int_to_bitarray(Count - Index).
+
+
+
+% -spec find_tree(integer(), random_access_list()) -> ral_node().
+% find_tree(_, []) -> nil;
+% find_tree(Index, [{Count, _, _, _} | T]) when Index > Count -> 
+%     find_tree(Index - Count, T);
+% find_tree(_, [H | _]) ->
+%     H.
+
+int_to_bitarray(Int) -> 
+    BinaryString = integer_to_list(Int, 2),
+    lists:map(fun ($1) -> 1; ($0) -> 0 end, BinaryString).
+
+
+-spec build_traversal_list(ral_node()) -> list(integer()).
 build_traversal_list(_) -> ok.
 
 
 
--spec find_using_traversal_list(list()) -> integer().
-find_using_traversal_list(_) -> ok.
+-spec find_using_traversal_list(list(), list(integer())) -> integer().
+find_using_traversal_list({_, Value, L, R}, [Instruction, T]) -> 
+    ok.
 
 
 % Recursive??
@@ -35,25 +51,24 @@ replace_using_traversal_list([]) -> ok.
 
 
 % Adds item to the beginning
--spec cons(integer(), list()) -> ok.
+-spec cons(integer(), random_access_list()) -> ok.
 cons(V, nil) -> 
-    cons(V, []);
+    [{1, V, nil, nil}];
 cons(V, []) -> 
     [{1, V, nil, nil}];
-cons(V, [{1, NV, nil, nil} | T]) -> 
-    [merge({1, V, nil, nil}, {1, NV, nil, nil}), T]; % Need to better handle mergeing for all even trees
+cons(V, [{Count, NV, nil, nil} | T]) -> 
+    [merge({1, V, nil, nil}, {Count, NV, nil, nil}), T]; 
 cons(V, Ral) ->
     {1, V, nil, nil} ++ Ral.
 
 
--spec merge(list(), list()) -> list().
-merge({Count, LV, LL, LR}, {Count, RV, RL, RR}) -> 
-    {Count + Count, {Count, LV, LL, LR}, {Count, RV, RL, RR}};
-merge({Count, LL, LR}, {Count, RL, RR}) -> 
-    {Count + Count, {Count, LL, LR}, {Count, RL, RR}}.
 
-
-
+-spec merge(ral_node(), ral_node()) -> ral_node().
+merge(Node, []) -> Node;
+merge({Count, LV, LL, LR}, [{Count, RV, RL, RR} | T]) -> 
+    merge({Count + Count, nil, {Count, LV, LL, LR}, {Count, RV, RL, RR}}, T);
+merge(Node, Ral) ->
+    Node ++ Ral.
 
 
 -ifdef(EUNIT).
