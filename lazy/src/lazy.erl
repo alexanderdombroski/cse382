30cb9b09c770dbfbe41ffa41066f021b919ee44d
alexanderdombroski <dom21013@byui.edu>
2024-11-20 14:09:15 -0700

    probably created a really good rnd number generator

--- lazy/src/lazy.erl
+++ lazy/src/lazy.erl
@@ -1,19 +1,21 @@
 -module(lazy).
 
--export([get_today/0, get_next_day/1, send_message/3, start/0, start/1, next/1, loop/1, stop/1]).
+-export([get_today/0, get_next_day/1, start_rnd_stream/1, start/0, start_date_stream/1, next/1, recieve_date/1, stop/1, get_lcg_rnd/1, random_sequence/2]).
 
-
-
-send_message(Pid,Message,Response_handler)->
-    Pid ! {self(),Message},
-    receive
-        Response -> Response_handler(Response)
-    end.
 
 
 get_today() -> 
     {Date, _} = calendar:local_time(),
     Date.
+
+get_lcg_rnd(Seed) -> 
+    math:fmod((Seed * 1664525 + 1013904223), math:pow(2, 32)).
+
+
+random_sequence(Seed, Count) when Count > 0 ->
+    [get_lcg_rnd(Seed) | random_sequence(get_lcg_rnd(Seed) + 2, Count - 1)];
+random_sequence(_, 0) ->
+        [].
 
 % Get the next day of the given day
 get_next_day(Date) ->
@@ -28,32 +30,56 @@ get_next_day(Date) ->
 % Start with today's date
 start() ->
     {Date, _} = calendar:local_time(),
-    start(Date).
+    {start_date_stream(Date), start_rnd_stream(1234)}.
 
 % Start with a specific date
-start(InitialDate) ->
-    spawn(fun() -> loop(InitialDate) end).
+start_date_stream(InitialDate) ->
+    spawn(fun() -> recieve_date(InitialDate) end).
+
+
+start_rnd_stream(Seed) -> 
+    spawn(fun() -> recieve_rnd(Seed) end).
+
 
 next(Pid) ->
     Pid ! {next, self()},
     receive
-        {date, Date} -> Date
+        {date, Date} -> Date;
+        {rnd, Rng} -> Rng
     end.
 
 % Stop the stream
 stop(Pid) ->
     Pid ! stop.
 
-% Internal loop function
-loop(CurrentDate) ->
+% Internal loop functions
+recieve_date(CurrentDate) ->
     receive
         {next, From} ->
             NextDate = get_next_day(CurrentDate),
             From ! {date, NextDate},
-            loop(NextDate);
+            recieve_date(NextDate);
         stop ->
             ok
     end.
+
+recieve_rnd(Seed) -> 
+    receive
+        {next, From} ->
+            NextRng = get_lcg_rnd(Seed + 2),
+            From ! {rnd, NextRng},
+            recieve_rnd(NextRng);
+        stop ->
+            ok
+    end.
+
+
+
+% RANDOM NUMBER GENERATOR LCG Linear COngruential Generator
+
+
+
+
 
 
 
@@ -63,29 +89,11 @@ loop(CurrentDate) ->
 -include_lib("eunit/include/eunit.hrl").
 
 date_stream_test_() ->
-    ?_test(
-        setup(
-            fun setup_stream/0,      % Setup function: initializes the stream
-            fun teardown_stream/1,   % Teardown function: stops the stream
-            fun test_logic/1         % Test logic: uses the stream
-        )
-    ).
-
-setup_stream() ->
-    lazy:start({2024, 11, 18}).  % Start the stream and return the Pid
-
-teardown_stream(Stream) ->
-    lazy:stop(Stream).  % Stop the stream process
-
-test_logic_(Stream) ->
     [
-        ?_assertEqual({2024, 11, 19}, lazy:next(Stream)),
-        ?_assertEqual({2024, 11, 20}, lazy:next(Stream)),
-        ?_assertEqual({2024, 11, 21}, lazy:next(Stream))
+        ?_assertEqual({2024, 11, 19}, get_next_day({2024, 11, 18})),
+        ?_assertEqual({2024, 2, 29}, get_next_day({2024, 2, 28})),
+        ?_assertEqual({2025, 3, 1}, get_next_day({2025, 2, 28}))
     ].
-
-
-
 
 
 
